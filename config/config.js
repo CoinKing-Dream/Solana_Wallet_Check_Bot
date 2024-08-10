367fec4cca13bc7bc1863395a19516f08026ada2
MitsuiMinoru <mitsuimino711@gamil.com>
2024-08-11 04:15:35 +1000

    .

---
+++ config/config.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+    TOKEN: process.env.TELEGRAM_TOKEN
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+const 
+    config = require("./config/config"),
+    TelegramBot = require('node-telegram-bot-api');
+    // bot = new TelegramBot(TOKEN, {polling: true});
+
+console.log(config.TOKEN);
+console.log("config.TOKEN");
+
