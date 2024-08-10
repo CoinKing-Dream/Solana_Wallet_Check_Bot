e1121b0ecda16c4b7cc9be178ba21c7a5f425b42
MitsuiMinoru <mitsuimino711@gamil.com>
2024-08-11 07:06:37 +1000

    update.

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const 
     config = require("./config/config"),
-    TelegramBot = require('node-telegram-bot-api');
+    express = require('express'),
+    cors = require('cors'),
+    http = require('http'),
+    TelegramBot = require('node-telegram-bot-api'),
+    axios = require("axios");
     // bot = new TelegramBot(TOKEN, {polling: true});
+const solPriceAPI = "https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd";
+let solPrice;
 
-console.log(config.TOKEN);
-console.log("config.TOKEN");
+const app = express();
+app.use(cors());
 
+async function fetchData() {
+    solPrice = (await axios.get(solPriceAPI)).data.solana.usd;
+
+    console.log(config.TOKEN);
+    console.log(solPrice);
+}
+fetchData();
+
+const server = http.createServer(app);
+server.listen(process.env.PORT, () => {
+    console.log('server is listening');
+});
